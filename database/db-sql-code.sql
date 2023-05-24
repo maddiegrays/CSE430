7fbb48d870ddc3f18a78a8126f940c6c39767b31
maddiegrays <madisongrayson@gmail.com>
2023-05-24 16:03:50 -0600

    save

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340_database_c3j5_user;
+    OWNER TO cse340_database_ndus_user;
 
 
 -- Table structure for table `classification`
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -3,11 +3,13 @@ const pool = require("../database/")
 /* ***************************
  *  Get all classification data
  * ************************** */
-async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+async function getClassifications()  { 
+  return await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+    )
 }
 
-module.exports = {getClassifications}
+module.exports = {getClassifications} 
 
 
 /* ***************************
@@ -16,11 +18,12 @@ module.exports = {getClassifications}
 async function getVehiclesByClassificationId(classification_id) {
   try {
     const data = await pool.query(
-      "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.classification_id = $1",
+      "SELECT * FROM public.inventory AS i JOIN public.classification AS c ON i.classification_id = c.classification_id WHERE i.classification_id = $1", 
       [classification_id]
     )
-    return data.rows
-  } catch (error) {
-    console.error("getclassificationsbyid error " + error)
+    return data.rows } catch (error) { console.error("getclassificationsbyid error " + error)
+   
   }
+
 }
+module.exports = {getClassifications, getInventoryByClassificationId};
