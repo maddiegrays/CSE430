4619e9b98175f27fc28eab8fbe42f6603b37dc32
maddiegrays <madisongrayson@gmail.com>
2023-05-27 11:44:35 -0600

    save

--- server.js
+++ server.js
@@ -5,21 +5,21 @@
 /* ***********************
  * Require Statements
  *************************/
+
+// This is the one shown in the video by prof  https://www.youtube.com/watch?v=KESjrocakuI
 const express = require("express")
-const env = require("dotenv").config()
-const app = express()
 const expressLayouts = require("express-ejs-layouts")
-const baseController = require("./controllers/baseController")
-
-
-
+require("dotenv").config()
+const app = express()
+const baseController = require("./controllers/baseController") //Added a new require statement to bring the base controller into scope
+const utilities = require("./utilities/")  // Added this line to bring the utilities into scope
 
 /* ***********************
  * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")
 
 
 /* ***********************
@@ -27,11 +27,42 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(require("./routes/static"))
 
-// Index route
-app.get("/", baseController.buildHome)
+/*Example of the route to deliver the home view*/
+//This is the initial route that was used
+// app.get("/", function(req, res) {
+//   res.render("index", {title: "Home"})
+// })
 
-// Inventory routes
+//Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+//Inventory routes
 app.use("/inv", require("./routes/inventoryRoute"))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'});
+});
+
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+* Error Handling
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+
 
 /* ***********************
  * Local Server Information
